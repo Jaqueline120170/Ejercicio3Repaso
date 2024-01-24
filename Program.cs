af52ae9621464e939669e194b14df013fd915ea0
Jaqueline Romero <csi23-jromres@colegioaltair.net>
2024-01-24 20:33:36 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio3Repaso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int pesoXsaco;
+            Console.WriteLine("Introduzca el peso de cada saco en número entero:");
+            pesoXsaco = Convert.ToInt32(Console.ReadLine());
+
+            int numeroSacos;
+            numeroSacos = 4000 / pesoXsaco;
+            Console.WriteLine("Usted puede cargar " +  numeroSacos + " sacos en la furgoneta");
+        }
+    }
+}
